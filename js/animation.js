3f6059beb7b3cdfdd349f1ab4f10543b425c312b
sergiomaddaleno <smadalenofigueroa@gmail.com>
2025-06-21 13:22:49 +0200

    changes

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Sergio Madaleno Figueroa"];
+const textArray = [""];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
